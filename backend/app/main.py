32de2e9c809f3e8fe0eb9b752c23def5412881b1
Harshitha M <Harshitha.Mullapudi@Triconinfotech.com>
2025-11-13 19:23:57 +0530

    updated main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-from backend.app.api import auth
-
+from backend.app.db.session import engine, Base
+from backend.app.db import models
 
 app = FastAPI()
 
+# Create tables automatically
+Base.metadata.create_all(bind=engine)
+
+# Routers
+from backend.app.api import auth
 app.include_router(auth.router, prefix="/auth")
